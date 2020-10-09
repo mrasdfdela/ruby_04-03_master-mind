5e7740def9b33b224095ddec3a780c33fc7184e3
mrasdfdela <jerryhsu830@gmail.com>
2020-10-09 18:40:45 -0400

    syntax adjustments

--- master-mind.rb
+++ master-mind.rb
@@ -1,39 +1,35 @@
 require 'byebug'
 
 class Mastermind
-  # variables
-    # available colors
-    attr_reader :game_over, :num_colors, :board, :code
+  # Mastermind class
+  attr_reader :game_over, :num_colors, :board, :code
 
-  # start game
-    # initialize maker - select color pattern
-    # create color counter
   def initialize()
     @game_over = false
-    
-    @@colors = ["White", "Black", "Red", "Green", "Orange", "Yellow"]
-    @num_colors = get_num_colors
+
+    @colors = %w[White Black Red Green Orange Yellow]
+    @num_colors = select_num_colors
     @code = Code.new(num_colors, @@colors)
 
-    board_size = get_board_size
+    board_size = select_board_size
     @board = Board.new(board_size)
   end
 
-  def get_num_colors
-    num_colors = [4,5,6]
-    msg = "How many colors would you like to play with? Select a number between 4 & 6:"
+  def select_num_colors
+    num_colors = [4, 5, 6]
+    msg = 'How many colors would you like to play with? Select a number between 4 & 6:'
     valid_entry(msg,num_colors)
   end
 
-  def get_board_size
-    size_options = [8,10,12]
-    msg = "What size board would you like to play with? Select 8, 10, or 12:"
+  def select_board_size
+    size_options = [8, 10, 12]
+    msg = 'What size board would you like to play with? Select 8, 10, or 12:'
     valid_entry(msg, size_options)
   end
-  
+
   def valid_entry(msg,arr)
     valid_size = false
-    invalid_msg = ""
+    invalid_msg = ''
 
     until valid_size == true
       puts invalid_msg + msg
@@ -42,7 +38,7 @@ class Mastermind
         valid_size = true
         return num
       end
-      invalid_msg = "Invalid entry. "
+      invalid_msg = 'Invalid entry. '
     end
   end
 
@@ -50,8 +46,7 @@ class Mastermind
     @@colors
   end
 
-  def play_turn
-
+  def play_turn 
   end
 
   def play
@@ -65,6 +60,7 @@ class Mastermind
 end
 
 class Hole
+  # Holes within the board
   attr_accessor :color
   def initialize(color=nil)
     @color = color
@@ -72,17 +68,17 @@ class Hole
 end
 
 class Code
+  # Secret code (auto-generated or user-specified)
   attr_reader :secret, :colors
-  
+
   def initialize(num,colors)
-    
     until Mastermind.colors.length == num
       Mastermind.colors.pop
     end
 
     @secret = code(colors)
   end
-  
+
   def code(colors)
     arr = Array.new
     4.times { |i|
@@ -90,11 +86,9 @@ class Code
     }
     return arr
   end
-
 end
 
 class Maker
-  
 end
 
 class Guesser
@@ -102,9 +96,8 @@ class Guesser
 end
 
 class Board
+  # Where the board, secret code, and guesses are kept
   attr_accessor :board
-  # print complete board, guesses, and correct placements/correct colors
-  # location of 
   def initialize(guesses)
     @board = Array.new(guesses) {
       Array.new(4) {
@@ -117,7 +110,7 @@ class Board
     first_row = "C: "
     code.each {|el| first_row += " #{el}"}
     puts first_row
-    
+
     board.each_with_index do |row, idx|
       i = (idx + 1).to_s
       i = " #{i}" if i.length < 2
